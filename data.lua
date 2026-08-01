37946cd7b73cb1329d9ee147b175626c4e0f5388
Em Karisch <emii.khaos@gmail.com>
2026-08-01 09:28:44 +0200

    feat: add tin plate

--- data.lua
+++ data.lua
@@ -2,3 +2,6 @@ require("__khaostin__.prototypes.particles.tin-ore-particle")
 require("__khaostin__.prototypes.entity.resources.tin-ore")
 
 require("__khaostin__.prototypes.item.tin-ore")
+require("__khaostin__.prototypes.item.tin-plate")
+
+require("__khaostin__.prototypes.recipe.tin-plate")
